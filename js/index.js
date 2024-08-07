79fde04217dfce152c0e242898d2aa0e6f53922b
JPSilva1989 <silvajuanpablo92@gmail.com>
2024-08-07 10:55:37 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,8 @@ const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
 
 const agregarEmail = async (data) => {
     const email = {
-        email: data
+        email: data,
+        client: "naum"
     }
     console.log(email)
     const options = {
